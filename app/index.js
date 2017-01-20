401634d2c49525695d386121cf44589016367e7b
Jeff G <jeffg@Jeffs-MacBook-Pro.local>
2017-01-19 19:59:15 -0500

    .

---
+++ app/index.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { LoginButton } from 'react-native-fbsdk'
+
+export default class ReactModoro extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome!
+        </Text>
+
+        <LoginButton
+          onLoginFinished={() => ({})}
+          onLogoutFinished={() => ({})}
+          defaultAudience='everyone' />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+})
---
+++ index.android.js
@@ -0,0 +1,4 @@
+import ReactKappa from './app'
+import { AppRegistry } from 'react-native'
+
+AppRegistry.registerComponent('ReactKappa', () => ReactKappa)
